a64c0cff4a2da480eaa4d762acf48b4e3e11c93b
Anh Minh Tran <anhminhtran235@gmail.com>
2021-09-19 08:22:31 -0700

    Support query using label

--- src/routes/images.js
+++ src/routes/images.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { User, Image, Label } = require('../models');
 const { check, validationResult } = require('express-validator');
+const { QueryTypes } = require('sequelize');
+const { sequelize } = require('../models/index');
 
 const { defaultExpressErrorHandler } = require('../util');
 const auth = require('../middleware/auth');
@@ -13,21 +15,68 @@ const {
 
 const { detectLabels } = require('../AmazonRecoknition/AmazonRecoknition');
 
-// Get images with pagination
+// Get images with pagination and query
 router.get('/', async (req, res) => {
   try {
-    const { offset, limit } = req.query;
-
-    const option = { include: 'user', order: [['createdAt', 'DESC']] };
-    if (offset) {
-      option.offset = +offset;
+    let { offset, limit, searchText, label } = req.query;
+    if (!offset) {
+      offset = 0;
     }
-    if (limit) {
-      option.limit = +limit;
+    if (!limit) {
+      limit = 10000000000000;
     }
 
-    const images = await Image.findAll(option);
-    return res.json(images);
+    let queryString = `SELECT \`Image\`.\`uuid\` as uuid,
+                                \`Image\`.\`filename\` as filename,
+                                \`Image\`.\`awskey\` as awsKey,
+                                \`Image\`.\`createdat\` as createdAt,
+                                \`Image\`.\`updatedat\` as updatedAt,
+                                \`user\`.\`uuid\`                   AS \`user.uuid\`,
+                                \`user\`.\`name\`                   AS \`user.name\`,
+                                \`user\`.\`createdat\`              AS \`user.createdAt\`,
+                                \`user\`.\`updatedat\`              AS \`user.updatedAt\`
+                                FROM   \`images\` AS \`Image\`
+                                LEFT OUTER JOIN \`users\` AS \`user\`
+                                            ON \`Image\`.\`userid\` = \`user\`.\`id\`
+                                INNER JOIN ( \`image_label\` INNER JOIN \`labels\`
+                                                    ON \`labels\`.\`id\` = \`image_label\`.\`labelid\`)
+                                        ON \`Image\`.\`id\` = \`image_label\`.\`imageid\``;
+    if (searchText) {
+      queryString += `WHERE  ( \`user\`.\`name\` LIKE '%${searchText}%'
+      OR \`Image\`.\`filename\` LIKE '%${searchText}%' )`;
+    }
+    if (label) {
+      queryString += `AND \`labels\`.\`name\` = '${label}'`;
+    }
+
+    queryString += ` ORDER  BY \`Image\`.\`createdat\` DESC
+    LIMIT  ${offset}, 10000000000000;`;
+
+    const images = await sequelize.query(queryString, {
+      type: QueryTypes.SELECT,
+    });
+
+    const beautifiedImages = [];
+    images.forEach((image) => {
+      const beautifiedImage = {};
+      beautifiedImage.uuid = image.uuid;
+      beautifiedImage.filename = image.filename;
+      beautifiedImage.awsKey = image.awsKey;
+      beautifiedImage.createdAt = image.createdAt;
+      beautifiedImage.updatedAt = image.updatedAt;
+      beautifiedImage.url = getCloudFrontUrl(beautifiedImage.awsKey);
+
+      beautifiedImage.user = {};
+      beautifiedImage.user.uuid = image['user.uuid'];
+      beautifiedImage.user.name = image['user.name'];
+      beautifiedImage.user.createdAt = image['user.createdAt'];
+      beautifiedImage.user.updatedAt = image['user.updatedAt'];
+
+      beautifiedImages.push(beautifiedImage);
+    });
+    console.log(beautifiedImages);
+
+    return res.json(beautifiedImages);
   } catch (error) {
     return defaultExpressErrorHandler(res, error);
   }
