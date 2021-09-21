47777e99c4acdb8774f6baea8f7810aedbc111bb
Anh Minh Tran <anhminhtran235@gmail.com>
2021-09-21 04:38:24 -0700

    Prepare for deployment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { sequelize } = require('./models');
+const path = require('path');
 
 sequelize
   .authenticate()
@@ -17,5 +18,17 @@ app.use(express.urlencoded({ limit: '50mb' }));
 app.use('/users', require('./routes/users'));
 app.use('/images', require('./routes/images'));
 app.use('/search', require('./routes/search'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
 
 module.exports = app;
